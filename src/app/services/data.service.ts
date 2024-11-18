39431502260f4e10f108d90687a1a5a6a5e75711
vasgat <vasgat@gmail.com>
2024-11-18 12:11:57 +0100

    update angular version to 18

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,5 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class DataService {
